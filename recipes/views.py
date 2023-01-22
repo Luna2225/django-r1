fd57f7453d581df6753a31286f895370b1a18448
lunnasantanna981@gmail.com <lunnasantanna981@gmail.com>
2023-01-22 11:52:23 -0300

    criando pagina receita

---
+++ recipes/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+def home(request):
+    return render(request,'recipes/pages/home.html', context={'name': 'Luana'})
+
+def receita(request):
+    return render(request,'recipes/portials/receita.html', context={'name': 'Luana'})
+
+   
+# def receita(request):
+#     return HttpResponseRedirect('recipes/portials/receita.html')
+
+def sobre(request):
+    return HttpResponse('SOBRE - hello django')
+
+def contato(request):
+    return HttpResponse('CONTATO - hello django')
+# Create your views here.
